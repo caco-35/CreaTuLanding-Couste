181257d284d399c8412541e07a761d95d2846e9b
Carlos Couste <cacotrico@gmail.com>
2024-12-16 16:51:10 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import './App.css'
+
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
